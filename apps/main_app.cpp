cb9142b76e1c9d3ef5ebbd080b1a4f66d050a080
Aditya-Bhatia <adityab.2610@gmail.com>
2024-05-12 19:56:04 -0700

    sticky panel

--- apps/main_app.cpp
+++ apps/main_app.cpp
@@ -398,7 +398,14 @@ public:
     glEnd();
 
     if (gui_enabled) {
-      ImGui::SetNextWindowSizeConstraints(ImVec2(450, 400), ImVec2(FLT_MAX, FLT_MAX));
+      const float DISTANCE = 10.0f;
+      static int corner = 0;
+      ImGuiIO& io = ImGui::GetIO();
+      ImGui::SetNextWindowSizeConstraints(ImVec2(io.DisplaySize.x / 4, io. DisplaySize.y - 2 * DISTANCE), ImVec2(MAXFLOAT, io. DisplaySize.y - 2 * DISTANCE));
+      ImVec2 window_pos = ImVec2((corner & 1) ? io.DisplaySize.x - DISTANCE : DISTANCE, (corner & 2) ? io.DisplaySize.y - DISTANCE : DISTANCE);
+      ImVec2 window_pos_pivot = ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
+      ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+
       if (ImGui::Begin("Control Panel", NULL)) {
 
         ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None; 
