c079fa06f16e8460e3146cec18490b4c71798e8e
Aditya-Bhatia <adityab.2610@gmail.com>
2025-09-19 16:51:55 -0400

    Double clickiong scroll wheel to delete all points now also adds a log statement

--- extern/tfn/widget.h
+++ extern/tfn/widget.h
@@ -398,6 +398,7 @@ inline tfn::vec4f TransferFunctionWidget::draw_tfn_editor__alpha_control_points(
       current_alphapoints->insert(current_alphapoints->begin(), AlphaPoint(vec2f(0.00f, 0.00f)));
       current_alphapoints->insert(current_alphapoints->begin() + 1, AlphaPoint(vec2f(1.00f, 0.00f)));
       current_gaussianobjects->clear();
+      set_log_text("Deleting all points and gaussians");
       deletePoints = false;
       tfn_changed = true;
     }
@@ -937,6 +938,7 @@ inline tfn::vec4f TransferFunctionWidget::draw_tfn_gaussian_alpha_control_points
       current_alphapoints->insert(current_alphapoints->begin(), AlphaPoint(vec2f(0.00f, 0.00f)));
       current_alphapoints->insert(current_alphapoints->begin() + 1, AlphaPoint(vec2f(1.00f, 0.00f)));
       current_gaussianobjects->clear();
+      set_log_text("Deleting all points and gaussians");
       deletePoints = false;
       tfn_changed = true;
     }
